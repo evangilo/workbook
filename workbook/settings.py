497a79eeab8371f13cf8b65114ca3040b69882fa
evangilo <evangilo@gmail.com>
2014-08-31 23:21:44 -0300

    Criando layout da tela inicial

--- workbook/settings.py
+++ workbook/settings.py
@@ -78,6 +78,7 @@ SOCIAL_AUTH_PIPELINE = (
     'social.pipeline.user.get_username',
     'social.pipeline.mail.mail_validation',
     'social.pipeline.user.create_user',
+    'social.pipeline.social_auth.associate_by_email',
     'social.pipeline.social_auth.associate_user',
     'social.pipeline.social_auth.load_extra_data',
     'social.pipeline.user.user_details',
