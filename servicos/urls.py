b2ae7acaf652732485594eaa814fa1aa3871a11b
Roldão Jr <roldaogjr@gmail.com>
2014-09-08 22:45:28 -0300

    Atualizao nome das views

--- servicos/urls.py
+++ servicos/urls.py
@@ -6,42 +6,34 @@ urlpatterns = patterns(
     url(r'^$', 'views.home', name='home'),
 
     url(r'categoria/adicionar$', 'categoria.adicionar',
-        name='categorias_adicionar'),
+        name='categoria_adicionar'),
     url(r'categoria/$', 'categoria.listar',
-        name='categorias_lista'),
+        name='categoria_listar'),
     url(r'categoria/excluir/(?P<id>\d+)$', 'categoria.excluir',
-        name='categorias_excluir'),
+        name='categoria_excluir'),
     url(r'categoria/editar/(?P<id>\d+)$', 'categoria.editar',
-        name='categorias_editar'),
+        name='categoria_editar'),
 
     url(r'servico/adicionar$', 'servico.adicionar',
-        name='servicos_adicionar'),
-
+        name='servico_adicionar'),
     url(r'servico/$', 'servico.listar',
-        name='servicos_lista'),
-
+        name='servico_listar'),
     url(r'servico/excluir/(?P<id>\d+)$', 'servico.excluir',
-        name='servicos_excluir'),
-
+        name='servico_excluir'),
     url(r'servico/editar/(?P<id>\d+)$', 'servico.editar',
-        name='servicos_editar'),
+        name='servico_editar'),
 
     url(r'avaliacao/comentarios/(?P<id>\d+)$', 'avaliacao.Comentarios',
         name='comentarios'),
-
     url(r'servico/descricao_servico/(?P<id>\d+)$', 'servico.Descricao_servico',
         name='servico_detalhes'),
 
     url(r'avaliacao/adicionar$', 'avaliacao.adicionar',
-        name='avaliacoes_adicionar'),
-
-     url(r'avaliacao/$', 'avaliacao.listar',
-        name='avaliacoes_lista'),
-    
-     url(r'avaliacao/excluir/(?P<id>\d+)$', 'avaliacao.excluir',
-        name='avaliacoes_excluir'),
-
+        name='avaliacao_adicionar'),
+    url(r'avaliacao/$', 'avaliacao.listar',
+        name='avaliacao_listar'),
+    url(r'avaliacao/excluir/(?P<id>\d+)$', 'avaliacao.excluir',
+        name='avaliacao_excluir'),
     url(r'avaliacao/editar/(?P<id>\d+)$', 'avaliacao.editar',
-        name='avaliacoes_editar'),
- 
+        name='avaliacao_editar'),
 )
