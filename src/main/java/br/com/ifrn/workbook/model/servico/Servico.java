b4eef5ff95324fd7f583de5c1fa3de672d037621
evangilo <evangilo@gmail.com>
2015-03-25 15:10:33 -0300

    adicionando annotaion Lob na coluna image

--- src/main/java/br/com/ifrn/workbook/model/servico/Servico.java
+++ src/main/java/br/com/ifrn/workbook/model/servico/Servico.java
@@ -6,6 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 
 import br.com.ifrn.workbook.model.BaseEntity;
@@ -36,6 +37,7 @@ public class Servico extends BaseEntity<Long>{
 	
 	private String celular;
 	
+	@Lob	
 	private byte[] image;
 	
 	private int media;
